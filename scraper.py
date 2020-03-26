74e882a352b4e7ac02467671549db3ef979d36ab
Andreas Baumgartner <mail@andreas.bz.it>
2020-03-26 10:53:50 +0100

    Neue Datenzeile "Aktuell positiv getestet"

--- scraper.py
+++ scraper.py
@@ -26,15 +26,17 @@ zeilen = list(map(bereinigen, html.split('\n')))
 x1 = zeilen[0]
 x2 = zeilen[1]
 x3 = zeilen[2]
-positive = zeilen[3]
-geheilt = zeilen[4]
-tot = zeilen[5]
+x4 = zeilen[3]
+positive = zeilen[4]
+aktuell_positive = zeilen[5]
+geheilt = zeilen[6]
+tot = zeilen[7]
 
 
 with open('/tmp/covid19_bz.csv', mode='w') as datei:
     # CSV-Parameter setzen
     writer = csv.writer(datei, delimiter=';', quoting=csv.QUOTE_ALL)
-    writer.writerow(['datum','positiv_gesamt','positiv_delta','geheilt_gesamt','geheilt_delta','tot_gesamt','tot_delta'])
+    writer.writerow(['datum','positiv_gesamt','positiv_delta','positiv_aktuell_gesamt','positiv_aktuell_delta','geheilt_gesamt','geheilt_delta','tot_gesamt','tot_delta'])
     
     # Tage durchloopen
     for i in range(1, len(x1)):
@@ -43,13 +45,15 @@ with open('/tmp/covid19_bz.csv', mode='w') as datei:
 
         # tägliche Gesamtzahlen
         p = positive[i]
+        ap = aktuell_positive[i]
         g = geheilt[i]
         t = tot[i]
 
         # tägliche Deltas berechnen
         diff_p = (positive[i] - positive[i-1]) if type(positive[i-1]) == int else positive[i]
+        diff_ap = (aktuell_positive[i] - aktuell_positive[i-1]) if type(aktuell_positive[i-1]) == int else aktuell_positive[i]
         diff_g = (geheilt[i] - geheilt[i-1]) if type(geheilt[i-1]) == int else geheilt[i]
         diff_t = (tot[i] - tot[i-1]) if type(tot[i-1]) == int else tot[i]
 
         # CSV-File schreiben
-        writer.writerow([datum, p, diff_p, g, diff_g, t, diff_t])
+        writer.writerow([datum, p, diff_p, ap, diff_ap, g, diff_g, t, diff_t])
