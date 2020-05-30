abaumg/covid19-bz-scraper

commit 6e85a1738796007b850e114fff30302a2c453d73

updated parsing rules for positive_delta, added more robust number detection

scrape_pressreleases.py
@@ -118,6 +118,8 @@ def get_numbers_from_pressrelease(url, date=datetime.today().strftime('%Y-%m-%d'
             key = key.lower()
             value = value.strip()
             value = value.replace('\xa0', ' ')
+            value = value.replace('.', '')
+            value = re.sub('[\(\[].*?[\)\]]', '', value)
         except ValueError:
             continue
 
@@ -130,7 +132,7 @@ def get_numbers_from_pressrelease(url, date=datetime.today().strftime('%Y-%m-%d'
         elif 'getesteten personen' in key:
             fieldname = 'tested_total'
             value = value
-        elif 'positiv getestete neue' in key:
+        elif 'positiv getestet' in key and 'neu' in key:
             fieldname = 'positive_delta'
             value = value
         elif 'gesamtzahl' in key and 'infizierte personen' in key:

commit a2e71beb5325c67a8624e54802c4c4268ef10646

fixed bug which was introduced in 6e85a1738796007b850e114fff30302a2c453d73

scrape_pressreleases.py
@@ -161,14 +161,13 @@ def get_numbers_from_pressrelease(url, date=datetime.today().strftime('%Y-%m-%d'
             value = value
         elif 'geheilte' in key:
             fieldname = 'recovered_total'
-            matches = re.search(r"Insgesamt: +([0-9]{1,})(.*)", value)
-            value = matches.groups()[0]
+            value = value
         elif 'positiv' in key and 'sanitätsbetrieb' in key:
             fieldname = 'positive_sabes_employees'
-            value = value.split(' ')[0]
+            value = value
         elif 'getestete' in key and 'ärzte' in key:
             fieldname = 'positive_familydoctors'
-            value = value.split(' ')[0]
+            value = value
 
         else:
             # Unknown field